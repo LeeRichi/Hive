5fb8dc0cd7f8695c93ffd0ad1afd73025837c5c8
Chi Lee <chlee2@c2r2p13.hive.fi>
2024-05-16 17:20:01 +0300

    push

--- libftprintf_github/ft_printf.h
+++ libftprintf_github/ft_printf.h
@@ -6,16 +6,16 @@
 /*   By: chlee2 <chlee2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 21:04:53 by chlee2            #+#    #+#             */
-/*   Updated: 2024/05/14 18:40:44 by chlee2           ###   ########.fr       */
+/*   Updated: 2024/05/16 14:19:44 by chlee2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft/libft.h"
 # include <stdarg.h>
 # include <limits.h>
+# include <unistd.h>
 
 int	ft_printf(const char *format, ...);
 int	printchar(char c);
