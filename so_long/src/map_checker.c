d2efa9ff606fca864f2bc0a4336b230f4cb5fd22
Rich <applerich0306@gmail.com>
2024-09-06 13:23:41 +0300

    toilet

--- so_long/src/map_checker.c
+++ so_long/src/map_checker.c
@@ -6,17 +6,33 @@
 /*   By: chlee2 <chlee2@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 12:34:47 by chlee2            #+#    #+#             */
-/*   Updated: 2024/09/06 12:41:50 by chlee2           ###   ########.fr       */
+/*   Updated: 2024/09/06 13:23:30 by chlee2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
+
+//template
+//void  flood_fill(char **tab, t_point size, t_point begin)
+
 
 int	map_checker(t_game *game)
 {
 	//check the border
 
 	t_map **temp_map;
+	t_point size;
+	t_point begin;
+	unsigned int i;
 
-	temp_map = flood_fill(game->map, )
+
+	//run while loop until find the starting point, which is 0
+	i = 0;
+	while (game->map->cont[i][0] != 0 && i < game->map->cols)
+		i++;
+	begin = (t_point){i, 0};
+
+	size = (t_point){game->map->cols, game->map->rows};
+
+	temp_map = flood_fill(game->map, size, begin);
 }
