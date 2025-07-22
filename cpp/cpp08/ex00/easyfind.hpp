e725df0719e94e8828858ce0e2ba2e427a96fbd5
Rich <applerich0306@gmail.com>
2025-07-22 19:50:32 +0300

    UPDATED: added const version for easyfind

--- cpp/cpp08/ex00/easyfind.hpp
+++ cpp/cpp08/ex00/easyfind.hpp
@@ -13,4 +13,12 @@ typename T::iterator easyfind(T &container, int value) {
 		return it;
 }
 
-
+// Const version:
+template <typename T>
+typename T::const_iterator easyfind(const T &container, int value) {
+    auto it = std::find(container.begin(), container.end(), value);
+    if (it == container.end()) {
+        throw std::runtime_error("Value not found in the container");
+    }
+    return it;
+}
--- cpp/cpp08/ex00/main.cpp
+++ cpp/cpp08/ex00/main.cpp
@@ -7,9 +7,25 @@ int main() {
     try {
         auto it = easyfind(vec, 3);
         std::cout << "Found: " << *it << std::endl;
+        it = easyfind(vec, 0);
+        std::cout << "Found: " << *it << std::endl;
     } catch (const std::runtime_error &e) {
         std::cerr << e.what() << std::endl;
     }
+
+    //const version
+    try
+	{
+        const std::vector<int> vec = {1, 2, 3, 4, 5};
+		auto searched = easyfind(vec, 5);
+		std::cout << "Found: " << *searched << std::endl;
+		searched = easyfind(vec, -1);
+		std::cout << "Found: " << *searched << std::endl;
+	}
+	catch (std::runtime_error &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
     return 0;
 }
