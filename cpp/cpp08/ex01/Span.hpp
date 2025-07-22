476f51a01021bdc30f28eca6669cb88ebfd29d1e
Rich <applerich0306@gmail.com>
2025-07-22 19:22:40 +0300

    DONE: cpp08 ex01

--- cpp/cpp08/ex01/Span.hpp
+++ cpp/cpp08/ex01/Span.hpp
@@ -10,9 +10,18 @@ class Span {
 		Span();
 		Span(unsigned int n);
 		Span(const Span &og);
-		Span &operator=(Span &og);
+		Span &operator=(const Span &og);
 
-		void addNumber();
+		void addNumber(int n);
 		int shortestSpan() const;
 		int longestSpan() const;
+
+		template<typename InputIterator>
+		void addNumbers(InputIterator begin, InputIterator end)
+		{
+			if (std::distance(begin, end) + numbers.size() > max_size)
+				throw std::overflow_error("Error: Adding this range would exceed the span's capacity.");
+
+			numbers.insert(numbers.end(), begin, end);
+		}
 };
--- cpp/cpp08/ex01/main.cpp
+++ cpp/cpp08/ex01/main.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
 #include "Span.hpp"
+#include <numeric>
+
+// int main()
+// {
+// Span sp = Span(5);
+// sp.addNumber(6);
+// sp.addNumber(3);
+// sp.addNumber(17);
+// sp.addNumber(9);
+// sp.addNumber(11);
+// std::cout << sp.shortestSpan() << std::endl;
+// std::cout << sp.longestSpan() << std::endl;
+// return 0;
+// }
 
 int main()
 {
-Span sp = Span(5);
-sp.addNumber(6);
-sp.addNumber(3);
-sp.addNumber(17);
-sp.addNumber(9);
-sp.addNumber(11);
-std::cout << sp.shortestSpan() << std::endl;
-std::cout << sp.longestSpan() << std::endl;
-return 0;
+		Span sp = Span(10002);
+
+		int minVal = INT_MAX, maxVal = INT_MIN;
+
+		std::vector<int> randomVec;
+		for (int i = 0; i < 10000; ++i)
+		{
+			int val = rand() % 100000;
+			randomVec.push_back(val);
+			if (val < minVal) minVal = val;
+			if (val > maxVal) maxVal = val;
+		}
+
+		try {
+			sp.addNumbers(randomVec.begin(), randomVec.end());
+
+			std::cout << "Expected longest span: " << maxVal - minVal << std::endl;
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		}
+		catch (const std::exception &e) {
+			std::cerr << e.what() << std::endl;
+		}
 }
