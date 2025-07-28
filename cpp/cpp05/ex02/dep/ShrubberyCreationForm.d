db587ce22de513de257cfd86dd7d3df47d22fb97
Rich <applerich0306@gmail.com>
2025-07-28 23:03:20 +0300

    cleaned up cpp folder

--- cpp/cpp05/ex02/dep/ShrubberyCreationForm.d
+++
@@ -1,5 +0,0 @@
-obj/ShrubberyCreationForm.o: ShrubberyCreationForm.cpp \
- ShrubberyCreationForm.hpp AForm.hpp Bureaucrat.hpp
-ShrubberyCreationForm.hpp:
-AForm.hpp:
-Bureaucrat.hpp:
--- cpp/cpp05/ex02/dep/main.d
+++
@@ -1,7 +0,0 @@
-obj/main.o: main.cpp Bureaucrat.hpp ShrubberyCreationForm.hpp AForm.hpp \
- PresidentialPardonForm.hpp RobotomyRequestForm.hpp
-Bureaucrat.hpp:
-ShrubberyCreationForm.hpp:
-AForm.hpp:
-PresidentialPardonForm.hpp:
-RobotomyRequestForm.hpp:
